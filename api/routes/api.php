d9de22e084f2fbd8655f8be8efc124106a6de6c2
taha jaiti <kyojin.rar@gmail.com>
2025-04-16 17:25:58 +0100

    FEAT: Implement Campaign Sending, Preview, and Tracking
    This commit introduces functionality for sending campaigns, previewing them, and tracking opens.
    
    - Added API routes for sending campaigns, previewing campaigns, and tracking email opens.
    - Implemented unsubscribe functionality accessible via a signed route.
    - Reorganized API routes for better structure and readability.

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\SubscriberController;
+use App\Http\Controllers\TrackingController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-
-
-
-Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/unsubscribe/{subscriber}', [SubscriberController::class, 'unsubscribe'])->name('unsubscribe');
+Route::get('/track/open/{campaign_id}/{subscriber_id}', [TrackingController::class, 'trackOpen'])
+    ->name('track.open')
+    ->middleware('signed');
 
-Route::middleware('jwt')->group(function (){
-    Route::apiResource('campaigns', CampaignController::class);
+Route::middleware('jwt')->group(function () {
     Route::apiResource('newsletters', NewsletterController::class);
     Route::apiResource('subscribers', SubscriberController::class);
+    Route::apiResource('campaigns', CampaignController::class);
+    Route::post('/campaigns/{campaign}/send', [CampaignController::class, 'send']);
+    Route::get('/campaigns/{campaign}/preview', [CampaignController::class, 'preview']);
 });
