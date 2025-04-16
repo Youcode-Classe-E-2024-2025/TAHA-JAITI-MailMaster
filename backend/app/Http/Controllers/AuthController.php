4506a8d559fd9a6ba88f77d1ffc9afef2c5c041e
taha jaiti <kyojin.rar@gmail.com>
2025-04-16 11:28:29 +0100

    FEAT: defined the basic methods for login and register in the controller

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Res;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function login(Request $request){
+        $res = $this->authService->login($request);
+
+        return $res ? Res::success($res, 'Login successful') : Res::error('Login failed', 401);
+    }
+
+    public function register(Request $request){
+        $res = $this->authService->register($request);
+
+        return $res ? Res::success($res, 'Registration successful') : Res::error('Registration failed', 401);
+    }
+
+}
