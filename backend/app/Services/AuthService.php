91a9900c5b12abcafdcaa3f8167433b60a727bbf
taha jaiti <kyojin.rar@gmail.com>
2025-04-16 11:32:08 +0100

    FEAT: defined the methods for login and register in the service layer and defined the routes

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -4,12 +4,48 @@
 
 namespace App\Services;
 
-
-
-class AuthService {
-
+use App\Models\User;
+use Illuminate\Http\Request;
 
 
 
-    
+class AuthService
+{
+
+
+    public function register(Request $request)
+    {
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+
+        $token = $user->createToken();
+
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+
+
+    public function login(Request $request)
+    {
+        $creds = $request->only('email', 'password');
+
+        $user = User::where('email', $creds['email'])->first();
+        if (!$user || !password_verify($creds['password'], $user->password)) {
+            return null;
+        }
+        $token = $user->createToken();
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
 }
