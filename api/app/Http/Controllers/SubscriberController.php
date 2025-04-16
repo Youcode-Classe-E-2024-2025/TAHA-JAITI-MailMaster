c78773c888888f31ccbf2e4a9ee4cb0a5dbcf623
taha jaiti <kyojin.rar@gmail.com>
2025-04-16 16:15:22 +0100

    feat: Implement subscriber retrieval
    The `index` method in the `SubscriberController` now retrieves all subscribers and returns them in a standardized success response.  This allows clients to access the full list of subscribers.

--- api/app/Http/Controllers/SubscriberController.php
+++ api/app/Http/Controllers/SubscriberController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Res;
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
+
+    private 
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return Res::success(Subscriber::all(), 'Subscribers retrieved successfully');
     }
 
     /**
