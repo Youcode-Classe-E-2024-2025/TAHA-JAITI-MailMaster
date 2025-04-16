cda3d1bc9ea379c07a953f750e730b8e3115ef28
taha jaiti <kyojin.rar@gmail.com>
2025-04-16 12:10:32 +0100

    FEAT: defined the newsletter relationship in the user

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function newsletters()
+    {
+        return $this->hasMany(Newsletter::class);
+    }
 }
